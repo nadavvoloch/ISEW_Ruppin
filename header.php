413984f4da17036c9c50d8e58c24deb39ddf7fef
nadavvoloch <nvolloch@yahoo.com>
2024-02-09 12:03:18 +0200

    Add files via upload
    These are basic PHP files

---
+++ header.php
@@ -0,0 +1,9 @@
+<table>
+<tr>
+<td>
+This header will appear on all my web
+pages
+</td>
+</tr>
+</table>
+<br>
---
+++ page14.php
@@ -0,0 +1,7 @@
+<html>
+<head></head>
+<body>
+<?php include("header.php"); ?>
+This is the content of page 14. Bla Bla.
+</body>
+</html>
